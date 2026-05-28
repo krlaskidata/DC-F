8e715317d4fd3403031d4afa228f39716c98cb70
molaskidata <laura.kraft@skidata.com>
2026-05-28 15:51:31 +0200

    Add sys and os imports; update sys.path for module resolution

--- Services/zizi.py
+++ Services/zizi.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'BOT'))
+
 import discord
 from discord.ext import commands, tasks
 from discord.ui import View
